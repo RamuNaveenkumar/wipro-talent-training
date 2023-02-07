2edcf604f37e2e6aba52f0bdf323463e8a351813
RamuNaveenkumar <91104979+RamuNaveenkumar@users.noreply.github.com>
2023-02-07 09:41:19 +0530

    Create print_1_to_100.java

---
+++ Day 1/print_1_to_100.java
@@ -0,0 +1,12 @@
+public class print_1_to_100 
+{
+
+    public static void main(String[] args)
+    {
+        int a =1;
+        while(a<=100) {
+            System.out.println(a);
+            a++;
+        }
+    }
+}
