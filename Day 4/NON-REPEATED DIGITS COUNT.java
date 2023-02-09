bea8bfd165b316c034ae92c0549a08d60a3c8a2c
RamuNaveenkumar <91104979+RamuNaveenkumar@users.noreply.github.com>
2023-02-09 08:58:11 +0530

    Create NON-REPEATED DIGITS COUNT.java

---
+++ Day 4/NON-REPEATED DIGITS COUNT.java
@@ -0,0 +1,43 @@
+import java.io.*;
+
+import java.util.*;
+
+// Read only region start
+
+class UserMainCode
+
+{
+
+public int nonRepeatDigitsCount(int input1){
+
+// Read only region end
+
+int c=0,r,i;
+
+int h[]=new int[10];
+
+while(input1>0){
+
+r=input1%10;
+
+h[r]++;
+
+input1=input1/10;
+
+}
+
+for(i=0;i<10;i++){
+
+if(h[i]==1){
+
+c++;
+
+}
+
+}
+
+return c;
+
+}
+
+}
