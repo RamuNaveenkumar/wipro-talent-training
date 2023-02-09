98ac8bc0fc3bfa2e8741f26e42b74f16d3d79f70
RamuNaveenkumar <91104979+RamuNaveenkumar@users.noreply.github.com>
2023-02-09 09:02:49 +0530

    Create EVEN DIGIT'S SUM.java

---
+++ Day 4/EVEN DIGIT'S SUM.java
@@ -0,0 +1,35 @@
+import java.io.*;
+
+import java.util.*;
+
+// Read only region start
+
+class UserMainCode
+
+{
+
+public int EvenDigitsSum(int input1){
+
+// Read only region end
+
+int r,sum=0;
+
+while(input1>0){
+
+r=input1%10;
+
+if(r%2==0){
+
+sum=sum+r;
+
+}
+
+input1=input1/10;
+
+}
+
+return sum;
+
+}
+
+}
